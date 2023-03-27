f4d48229b3fce6147d45f9bd9899cb98495800e6
mark_wightman <mark_wightman@uri.edu>
2023-03-26 22:56:10 -0400

    facebook login

--- helloWorld.py
+++ helloWorld.py
@@ -1,3 +1,7 @@
-userName = input("Hello! Please enter your name!")
+userName = input("Username: ")
+userPassword = input("Password: ")
 
-print ("hello", userName, " nice to meet you")
+if(userName.lower() == "mark" and userPassword == "1234"):
+    print("Welcome to facebook!")
+else:
+    print("Wrong credentials, try again!")
